8872051b843760eb95ad307fcb7e45dd8e5e0947
Vyacheslav <rainboss1234@gmail.com>
2026-06-27 00:17:02 +0700

    small refactoring

--- src/main/java/ru/vych/http/impl/exceptions/HttpClientConfigurationException.java
+++ src/main/java/ru/vych/http/impl/exceptions/HttpClientConfigurationException.java
@@ -1,5 +1,8 @@
 package ru.vych.http.impl.exceptions;
 
+/**
+ * Исключение выбрасываемое клиентом в случае если возникла во время обработки его конфигурации
+ */
 public class HttpClientConfigurationException extends HttpClientException {
     public HttpClientConfigurationException(String message) {
         super(message);
